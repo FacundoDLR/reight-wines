6d79c1c474a6425c55efb1d0abe39806bb99a807
FlorStaricco <florenciastaricco@gmail.com>
2023-01-25 16:02:05 -0300

    retoque final CartItem

--- src/components/shopping/CartItem.js
+++ src/components/shopping/CartItem.js
@@ -2,6 +2,9 @@ import {useContext} from "react"
 import {ProductsContext} from "../../context/ProductsProvider"
 import styled from 'styled-components'
 import { Link } from "react-router-dom"
+import {IoTrashOutline} from 'react-icons/io5';
+import {IoIosAdd} from 'react-icons/io';
+import {IoMdRemove} from 'react-icons/io';
 
 
 const CartItem = ({item}) => {
@@ -14,39 +17,48 @@ const CartItem = ({item}) => {
 
     return (
         <ItemCarrito>
-            
+              
               <Image>
                   <img src={imagenes(`./${image}`)} alt={tipo} height="110px" width="auto"/>
               </Image>
               <Text>
-                  <Nombre><Text>{nombre} </Text></Nombre>
-                  <Tipo><Text>{tipo} 750ml</Text></Tipo>
+                <Nombre><Text>{nombre} </Text></Nombre>
+                <Tipo><Text>{tipo} 750ml</Text></Tipo>
               </Text>
-              <Precio>
-                  <div>${`${precio} x ${quantity}`}</div>
-                  <div> Total ${`${precio*quantity}`}</div>
-              </Precio>
-              <CounterContainer>
+              <ContentTwo>
+                <Cantidad>
+                  <Precio > <p>${`${precio}`} un.</p></Precio>
                   <ButtonContainer>
-                      <ButtonMenos onClick={()=>delFromCart(id)}>-</ButtonMenos>
-                      <ButtonMas onClick={()=>addToCart(id)}>+</ButtonMas>
+                    <ButtonMenos><IoMdRemove style={{ paddingTop:"13px", fontWeight: "bold" , color:"#ef8557"}} onClick={()=>delFromCart(id)} /></ButtonMenos>
+                    <ButtonCount >{`${quantity}`}</ButtonCount>
+                    <ButtonMas><IoIosAdd style={{ position:"relative", color:"#ef8557"}} onClick={()=>addToCart(id)}/></ButtonMas>
                   </ButtonContainer>
+                </Cantidad>
+
+                <CounterContainer>
                   <Link to="/carro" className="modCart">
                       <ButtonClear onClick={(all)=>delFromCart(id, all)}>
-                          Eliminar Todos
+                        <IoTrashOutline style={{fontSize:30, margin:"0px 0px", color: "var(--orange75)"}} />
+                          {/* Eliminar Todos */}
                       </ButtonClear>
                   </Link>
-              </CounterContainer>
-
+                  <Precio>
+                    <div> Total: ${`${precio*quantity}`}</div>
+                </Precio>
+                </CounterContainer>
+              </ContentTwo>
+            
         </ItemCarrito>
     )
 }
 
 export default CartItem
 
+
+
 const ItemCarrito = styled.div`
   width: 53vw;
-  display: flex; 
+  display: flex;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
@@ -64,42 +76,49 @@ const ItemCarrito = styled.div`
     align-items: center;
     gap: 5px;
 }
-
+@media screen and (max-width: 576px){
+  position: relative;
+  bottom: 20px;
+}
   `
-
-
  const Image = styled.div`
   width: 25%;
   height: 25%;
-  
+
   @media screen and (max-width: 768px){
    padding-right: 30%;
+   padding-top: 30px;
 }
-
-  @media screen and (max-width: 576px){
-   padding-right: 40%;
-}
+  
  `
 
-const Text = styled.p`
-    height: 25%;
-    width: 100%;
+const Text = styled.div`
     text-align: center;
     color: var(--dark65);
     position: relative;
+    right: 10px;
     display: flex;
     flex-direction: column;
     justify-content: space-evenly;
     align-items: center;
   
+  @media screen and (max-width: 768px){
+   padding-left: 10%;
+}
+  @media screen and (max-width: 576px){
+   padding-left: 15%;
+   }
 `
-
-
   const Nombre = styled.h3`
-  padding-top: 25px;
+  width: 180px;
   color: var(--dark);
   font-family: 'Carter One', cursive;
   font-size: 21px;
+
+  @media screen and (max-width: 576px){
+  position: relative;
+   top: 10px ;
+}
   `
 
   const Tipo = styled.p`
@@ -107,38 +126,87 @@ const Text = styled.p`
   font-family:'Satisfy', cursive;
   font-size: 18px;
   font-weight: 200;
+
+  @media screen and (max-width: 768px){
+   width: 100%;
+  }
   `
+
+  const ContentTwo = styled.div`
+    display: flex;
+    position: relative;
+    right: 80px;
+    bottom: 10px;
+
+
+   @media screen and (max-width: 768px){
+   padding-left: 25%;
+   gap:45px
+  }
+  @media screen and (max-width: 576px){
+    position: relative;
+    top: 30px;
+    right: 40%;
+  }
+  `
+  const Cantidad = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 35px;
+  position: relative;
+  left: 40px;
+  bottom: 10px;
+
+  @media screen and (max-width: 576px){
+    align-items: center;
+    justify-content: center;
+    left: 120px;
+    gap: 0px;
+    padding: 0px;
+    margin: 0px;
+    position: relative;
+    bottom: 100px;
+}
+`
   const Precio = styled.p`
+  width: 100%;
   font-family:'EB Garamond', serif;
   font-size: 18px;
   display: flex;
   flex-direction: column;
-  align-items: center;
   text-align: center;
-  position: relative;
-  top: 2px;
   color: var(--dark65);
-
-  @media screen and (max-width: 576px){
-    padding-right: 2%;
-  }
+  margin: 15px;
 
   div {
-  position: relative;
   width: 150px;
   font-weight: 600;
+
   }
+  p{
+    position: relative;
+    left: 25px;
+    top: -10px;
 
-
+    @media screen and (max-width: 576px){
+      position: relative;
+      top: 10px;
+      left: 38px;
+    }
+  }
   `
+
 const CounterContainer = styled.div`
-  height: 20%; 
+  height: 20%;
   text-align: center;
   display: flex;
   flex-direction: column;
   justify-content: center;
   gap: 0.3px;
-  padding-top: 15px;
+  position: relative;
+  left: 105px;
+  top:20px;
   div {
     display: flex;
     flex-direction: row;
@@ -147,76 +215,87 @@ const CounterContainer = styled.div`
   @media screen and (max-width: 768px){
    padding-right: 15%;
   }
-
   @media screen and (max-width: 576px){
-   padding-right: 20%;
+   position: relative;
+   top: -20px;
+   align-items: center;
+
   }
 `
+
 const ButtonContainer = styled.div`
     position: relative;
-    bottom: 10px;
-    left: 45px;
-    display: flex; 
-
+    left: 35px;
+    display: flex;
+    border-radius: 0.6rem;
+    border: solid 1px var(--dark80) ;
+    height: 33px;
+    width: 100px;
+    height: 25%;
+   
+    @media screen and (max-width: 576px){
+      position: relative;
+      bottom: 8px;
+    }
 
 `
 const ButtonMenos = styled.button`
-    background: var(--dark10);
-    color: var(--dark80);
-    font-size: 2rem;
-    width: 25px;
-    height: 25px;
-    margin: 5px;
-    border-radius: 0.5rem;
-    border: solid 1px  var(--dark80) ;
-    font-family: "Nunito", sans-serif;
-    font-size: 15px;
-    text-align: center;
+    background: transparent;
+    border: none ;
+    font-size: 25px;
     position: relative;
-    top: 15px;
-    
+    bottom: 14px;
+    left: 5px;
+    text-align: center;
+
     @media screen and (max-width: 1024px){
     &:active {
-    background-color: var(--dark35) !important;
-    color: var(--beige);
-    box-shadow: none;
-    transform: translateY(4px);
+    color: var(--dark65);
+    transform: translate(1.5px, 1.5px);
     transition: 0.15s;
-    }
   }
+}
 `
-
-const ButtonMas = styled.button`
-    background: var(--dark10);
-    color: var(--dark80);
+const ButtonCount = styled.button`
+    background: transparent;
+    color: var(--dark65);
     font-size: 2rem;
+    font-weight: bold;
     width: 25px;
     height: 25px;
     margin: 5px;
-    border-radius: 0.5rem;
-    border: solid 1px  var(--dark80) ;
-    font-family: "Nunito", sans-serif;
+    border: none ;
+    font-family: var(--subtitle-font);
     font-size: 15px;
-    text-align: center;
     position: relative;
-    top: 15px;
-    left: 10px;
-    
-   
+    bottom: 3px;
+    left: 5px;
+    text-align: center;
+
+`
+const ButtonMas = styled.button`
+    background: transparent;
+    border: none ;
+    font-size: 25px;
+    position: relative;
+    bottom: 8px;
+    left: 6px;
+    text-align: center;
+
+    @media screen and (max-width: 1024px){
     &:active {
-    background-color: var(--dark35) !important;
-    color: var(--beige);
-    box-shadow: none;
-    transform: translateY(4px);
+    color: var(--dark65);
+    transform: translate(1.5px, 1.5px);
     transition: 0.15s;
-    
   }
+}
 `
 
 const ButtonClear = styled.div`
-    color:white;
-    background-color: var(--orange);
+    color:var(--orange);
+    background-color: transparent;
     padding: 5px 10px;
+    padding-top: 10px;
     position: relative;
     top: 15px;
     left: 30px;
@@ -226,18 +305,20 @@ const ButtonClear = styled.div`
     font-size: 11px;
     text-transform: uppercase;
     text-align: center;
-    width:115px;
-    height: 28px;
-    box-shadow: 0 5px 5px 0 rgba(0,0,0,0.2), 0 5px 5px 0 rgba(0,0,0,0.19);
 
-  
+    @media screen and (max-width: 576px){
+      position: relative;
+      left: -45px;
+    }
+
   &:active {
     background-color: var(--orange) !important;
     color: var(--beige);
     box-shadow: none;
     transform: translateY(4px);
     transition: 0.15s;
-    
+
+
   }
 `
 
--- src/components/shopping/ShoppingCart.js
+++ src/components/shopping/ShoppingCart.js
@@ -4,7 +4,6 @@ import styled, { keyframes } from 'styled-components'
 import CartItem from "./CartItem"
 import {HiOutlineShoppingCart} from 'react-icons/hi';
 import {GiCheckMark} from 'react-icons/gi';
-import { useState } from 'react';
 import { Link } from "react-router-dom"
 
 
@@ -16,7 +15,10 @@ const ShoppingCart = () => {
 
 
   return (
-const ShoppingCart = () => {
+    <CarritoContainer>
+        <FondoBlanco>
+          <Titulo>
+            <HiOutlineShoppingCart style={{fontSize:35, margin:"0px 18px", color: "var(--orange75)"}}/>
             <h1>Carrito de Compras</h1>
           </Titulo>
 
@@ -40,7 +42,7 @@ const ShoppingCart = () => {
   )
 }
 export default ShoppingCart
-
+ 
 
 const fadeIn = keyframes`
 0% {
@@ -77,27 +79,50 @@ const FondoBlanco = styled.div` width: 80vw;
   margin-bottom: 10%;
   margin-top: 50px;
 `
+
 const Titulo = styled.div`
   width: 90%;
   height: 50px;
   display: flex;
   justify-content: center;
   border-bottom: 1px solid grey;
-  padding: 3.8%;
+  padding: 4%;
   position: relative;
   bottom: 15px;
-h1{
-  text-align: center;
-  color: var(--orange75);
-  font-size: 2.8rem;
-  font-weight: 600;
-  font-family:'EB Garamond', serif;
-}
+
+  @media screen and (max-width: 768px){
+    padding: 6%;
+  }
+
+  @media screen and (max-width: 576px){
+    padding: 8%;
+  }
+
+  h1{
+    text-align: center;
+    color: var(--orange75);
+    font-size: 2.8rem;
+    font-weight: 600;
+    font-family:'EB Garamond', serif;
+  }
 `
 const ContainerText = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  p{
+    color: var(--dark);
+    font-family:'Satisfy', cursive;
+    font-size: 20px;
+    position: relative;
+    top:20px;
+    
+  @media (max-width: 576px){
+    padding: 30px;
+    text-align: center;
+  }
+}
 `
 const ButtonComprando = styled.button`
   padding: 5px;
@@ -111,10 +136,12 @@ const ButtonComprando = styled.button`
   width:auto;
   height: 28px;
   box-shadow: 0 8px 8px 0 rgba(0,0,0,0.2), 0 6px 12px 0 rgba(0,0,0,0.19);
+
 @media screen and (min-width: 1024px){
 &:hover{background-color: #999898 !important;
   color: var(--beige) ;
   border: 1px solid #999898;}
+
 &:active {
   background-color: var(--orange) !important;
   box-shadow: none;
@@ -131,8 +158,10 @@ const ButtonsContainer = styled.div`
   gap: 20px;
   padding-top: 5px;
   margin-bottom: 25px;
+
   @media screen and (min-width: 1200px){
     gap: 150px;
+    
   }
 `
 const ButtonLimpiarCarro = styled.button` width: 200px;
@@ -149,8 +178,10 @@ const ButtonLimpiarCarro = styled.button` width: 200px;
   width:auto;
   height: 35px;
   box-shadow: 0 8px 8px 0 rgba(0,0,0,0.2), 0 6px 12px 0 rgba(0,0,0,0.19);
+
   @media screen and (min-width: 1024px){
   &:hover{background-color: #999898 !important;}}
+
   &:active {
   background-color: var(--orange) !important;
   box-shadow: none;
@@ -171,11 +202,13 @@ const ButtonFinalizarCompra = styled.button`
   width:auto;
   height: 35px;
   box-shadow: 0 8px 8px 0 rgba(0,0,0,0.2), 0 6px 12px 0 rgba(0,0,0,0.19);
+
 @media screen and (min-width: 1024px){
 &:hover{
   background-color: #999898 !important;
   color: var(--beige) ;
   border: 1px solid #999898;}
+
 &:active {
   background-color: var(--orange) !important;
   box-shadow: none;
@@ -183,61 +216,3 @@ const ButtonFinalizarCompra = styled.button`
   display: flex;}
   }
 `
-// const CounterContainer = styled.div`
-//   height: 20%;
-//   text-align: center;
-//   display: flex;
-//   flex-direction: row;
-//   justify-content: center;
-//   gap: 0.3px;
-//   padding-top: 15px;
-// `;
-/******CODIGO ANTERIOR ********/
-// const ShoppingCart = () => {
-  // const {clearCart, state} = useContext(ProductsContext);
-  
-  //   // useEffect(() => {
-  //   //   updateState()
-  //   // }, [])
-    
-  
-  //   /*  return (
-  //   <section>
-  //       <div>
-  //         <div>
-  //           <h1>Carrito de compras</h1>
-  //         </div>
-  //         <div>
-  //           {!state.cart[0] ? "No tienes productos en tu carrito" : state.cart.map((item) => <CartItem key={item.id} item={item} />)}
-  //         <div>
-  //           <button onClick={()=>clearCart()}>Limpiar Carrito</button>
-  
-  //           <button>Finalizar Compra</button>
-  //         </div>
-  //       </div>
-  //       </div>
-  //     </section>
-  //   )
-  // */
-  //   return (
-  //      <CarritoContainer>
-  //       <FondoBlanco>
-  //          <Titulo>
-  //          <h1>Carrito de compras</h1>
-  //          </Titulo>
-  //          <div>
-  //          {!state.cart[0] ? "No tienes productos en tu carrito" : state.cart.map((item) => <CartItem key={item.id} item={item} />)}
-  //          </div>
-  
-  //        <ButtonsContainer>
-  //           <ButtonLimpiarCarro onClick={clearCart}>Limpiar Carrito</ButtonLimpiarCarro>
-  
-  //            <ButtonFinalizarCompra>Finalizar Compra</ButtonFinalizarCompra>
-  //          </ButtonsContainer>
-  
-  //     </FondoBlanco>
-  //      </CarritoContainer>
-  //    )
-  // }
-  
-  // export default ShoppingCart
